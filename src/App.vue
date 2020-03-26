eda651a9e39125aa119fea6c21d38bae84066430
saibotk <git@saibotk.de>
2020-03-26 04:08:17 +0100

    Add tailwindcss-theming

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-background text-on-background min-h-screen flex flex-col">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view class="flex-grow"/>
+    <footer class="flex-0 flex h-32 justify-around bg-brand p-2 text-on-brand">
+      <div class="flex items-center">
+        <button @click="theme('light')" class="m-1 hover:text-on-brand-hover"><font-awesome-icon icon="sun" size="2x" /></button>
+        <button @click="theme('dark')" class="m-1 hover:text-on-brand-hover"><font-awesome-icon icon="moon" size="2x" /></button>
+      </div>
+    </footer>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'app',
+  data: () => ({
+    themes: ['light', 'dark']
+  }),
+  methods: {
+    theme (name?: string) {
+      document.body.removeAttribute('data-theme')
+
+      if (name) {
+        document.body.setAttribute('data-theme', name)
+      }
+    }
+  }
+})
+</script>
 
 <style lang="scss">
 #app {
@@ -14,19 +40,9 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
