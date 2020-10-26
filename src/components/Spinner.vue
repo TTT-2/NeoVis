53e755c8ca8c53fe4a2868a8f018d9ae9e7c48a7
saibotk <git@saibotk.de>
2020-10-26 03:21:04 +0100

    Adjust branch for repository links until merged

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,12 +1,12 @@
 <template>
-    <transition name="component-fade" mode="out-in" appear>
-      <div :key="'spinner'" v-if="syncedLoading" class="h-full w-full flex justify-center flex-col items-center">
-        <font-awesome-icon :icon="'spinner'" size="lg" class="transition-opacity m-2" spin></font-awesome-icon>
-      </div>
-      <div v-else>
-        <slot/>
-      </div>
-    </transition>
+  <transition name="component-fade" mode="out-in" appear>
+    <div :key="'spinner'" v-if="syncedLoading" class="h-full w-full flex justify-center flex-col items-center">
+      <font-awesome-icon :icon="'spinner'" size="lg" class="transition-opacity m-2" spin></font-awesome-icon>
+    </div>
+    <div v-else>
+      <slot/>
+    </div>
+  </transition>
 </template>
 
 <script lang="ts">
