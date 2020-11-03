7172d45cffc652dc4a2b69dba5f05bc5889853f3
saibotk <git@saibotk.de>
2020-11-03 20:25:43 +0100

    Adjust source link back to master branch

--- src/components/FunctionLine.vue
+++ src/components/FunctionLine.vue
@@ -2,7 +2,7 @@
   <div class="bg-gray-400 dark:bg-gray-900 text-gray-800 dark:text-gray-100 px-4 p-2 inline-flex items-center rounded-md shadow-md w-full">
     <neo-realm-indicator :realm="realm"></neo-realm-indicator>
     <span class="font-mono">{{ name }}(<span v-for="(param, index) in params" :key="index">{{ param.typs.join('|') + ' ' + param.name }}{{ (index != (params.length - 1)) ? ', ' : '' }}</span>)</span>
-    <a v-if="Object.entries(source).length !== 0" :href="'https://github.com/TTT-2/TTT2/blob/docu-fix' + source.file + '#L' + source.line" class="ml-auto">
+    <a v-if="Object.entries(source).length !== 0" :href="'https://github.com/TTT-2/TTT2/blob/master' + source.file + '#L' + source.line" class="ml-auto">
       <font-awesome-icon :icon="'external-link-alt'" class="ml-2 w-4 h-4" ></font-awesome-icon>
     </a>
   </div>
